9a542e73adfffcae150aee0f31dc6cf873273061
lordrichard <pauloreizinho@hotmail.com>
2019-07-01 17:56:10 +0200

    update

--- script.js
+++ script.js
@@ -1,80 +1,152 @@
-var cvs = document.getElementById('canvas');
-var ctx = cvs.getContext('2d');
+//Select the Canvas
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
-// Load images
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeDown = new Image();
+//Variables
+let frames = 0;
 
-bird.src = "images/bird.png";
-bg.src = "images/bg.png";
-fg.src = "images/fg.png";
-pipeUp.src = "images/pipeUp.png";
-pipeDown.src = "images/pipeDown.png";
+//Load main image
+const mainImg = new Image();
+mainImg.src = 'img/main.png';
 
-// Variables
-var gap = 85; //Gap between the pipes
-var constant = pipeUp.height + gap;
-
-let bX = 10; //Bird X position
-let bY = 150; //Bird Y position
-
-let gravity = 1.5;
-
-// On click
-function moveUp() {
-    bY -= 25;
-}
-document.addEventListener("keydown", moveUp)
-document.addEventListener("click", moveUp)
-document.addEventListener("touchstart", moveUp)
-
-// Pipe coordinates
-var pipe = [];
-pipe[0] = {
-    x : cvs.width,
-    y : 0
+//Game state
+const state = {
+    current: 0,
+    getReady: 0,
+    game: 1,
+    over: 2
 }
 
-// Draw pics
-function draw() {
-    // Show the images (image, X pos, Y pos)
-    ctx.drawImage(bg ,0 ,0);  
+//Control the Game
+function gameState(evt) {
+    switch(state.current) {
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            bird.flap();
+            break;
+        case state.over:
+            state.current = state.getReady;
+            break;
+    }
+}
+cvs.addEventListener('click', gameState)
+document.addEventListener('keydown', gameState)
+cvs.addEventListener('touchstart', gameState)
 
-    for (let i = 0; i < pipe.length; i++) {
-        ctx.drawImage(pipeUp ,pipe[i].x ,pipe[i].y);
-        ctx.drawImage(pipeDown ,pipe[i].x ,pipe[i].y + constant);
+//Backround
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
 
-        pipe[i].x--;
+    draw : function() {
+        ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
 
-        if (pipe[i].x == 100) {
-            pipe.push({
-                x : cvs.width,
-                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-            });
-        }
-        // Detect collision
-        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeUp.width &&
-            (bY <= pipe[i].y + pipeUp.height || bY + bird.height >= pipe[i].y + constant) ||
-            bY + bird.height >= cvs.height - fg.height) {
-                location.reload(); // Reload the page
-        }
-            
-        if (pipe[i].x == 5) {
-            score++;
-        }
+//Foreground
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+
+    draw : function() {
+        ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
     }
     
-
-    ctx.drawImage(fg ,0 ,cvs.height - fg.height);
-
-    ctx.drawImage(bird, bX, bY);
-    
-    bY += gravity; //Gravity animation
-
-    requestAnimationFrame(draw); //Show the animation
 }
 
-draw();
+//Bird
+const bird = {
+    animation : [
+        {sX: 276, sY: 112},
+        {sX: 276, sY: 139},
+        {sX: 276, sY: 164},
+        {sX: 276, sY: 139}
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+
+    frame: 0,
+
+    draw: function() {
+        let bird = this.animation[this.frame];
+
+        ctx.drawImage(mainImg, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h)
+    },
+
+    flap : function() {
+
+    },
+}
+
+//Start
+const getReady = {
+    sX: 0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width/2 - 173/2,
+    y: 80,
+
+    draw : function() {
+        if (state.current == state.getReady){
+            ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }   
+    }
+}
+
+//Game over
+const gameOver = {
+    sX: 175,
+    sY: 228,
+    w: 225,
+    h: 202,
+    x: cvs.width/2 - 225/2,
+    y: 90,
+
+    draw : function() {
+        if (state.current == state.over) {
+            ctx.drawImage(mainImg, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+    }
+}
+
+//Draw
+function draw() {
+    ctx.fillStyle = '#70c5ce';
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw();
+    fg.draw();
+    bird.draw();
+    getReady.draw();
+    gameOver.draw();
+}
+
+//Update
+function update() {
+
+}
+
+//Animation
+function loop() {
+    update();
+    draw();
+    frames++;
+
+    requestAnimationFrame(loop);
+}
+loop();
